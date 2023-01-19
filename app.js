e8fac00246605ea258633e6cce78cecbd2331ecf
Cumbie <bcumbie@una.edu>
2023-01-19 16:37:53 -0600

    refactor for env port var

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000;  
 
 app.get('/', (req, res) => {
   res.send('Hello World This is Barry 3!')
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
 
 console.log('in the node console');
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${ PORT }`)
 })
